fd913d7f5ad9a6a811a3b94af6e4ec7fb77d6484
Bennacer Douirat <your_verified_email@github.com>
2026-06-01 17:00:48 +0100

    redesigning
    q# Please enter the commit message for your changes. Lines starting

--- src/http/response/Response.java
+++ src/http/response/Response.java
@@ -1,3 +1,9 @@
+package http.response;
+
 public class Response {
-    
+    private String version; // which HTTP Rules apply to parsing the response.
+    private int status;
+    private String statusMessage;
+
+    private final Map<String, String> Headers = new HashMap<>();
 }
--- src/http/response/status/HttpStatusMessages.java
+++ src/http/response/status/HttpStatusMessages.java
@@ -1,4 +1,4 @@
-package http;
+package http.response.status;
 
 import java.util.Map;
 
