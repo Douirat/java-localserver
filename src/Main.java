656bccac854e99a102360e3e469f7ba4279c276c
oqritel <othmanqritel@gmail.com>
2026-08-13 16:38:46 +0100

    add simple tokenizer

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import java.io.IOException;
+import java.util.List;
 
 import config.ConfigLoader;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         String configFilePath = args[0];
         ConfigLoader configLoader = new ConfigLoader(configFilePath);
 
         try {
-            String configContent = configLoader.read();
-            System.out.println("Config Content:");
-            System.out.println(configContent);
+            List<String> tokens = configLoader.tokenize();
+            System.out.println("Tokens from config file:");
+            for (String token : tokens) {
+                System.out.println(token);
+            }
         } catch (IOException e) {
             System.err.println("Error reading config file: " + e.getMessage());
             System.exit(1);
--- src/config/ConfigLoader.java
+++ src/config/ConfigLoader.java
@@ -3,6 +3,8 @@ package config;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConfigLoader {
     private final Path configPath;
@@ -13,5 +15,32 @@ public class ConfigLoader {
 
     public String read() throws IOException {
         return Files.readString(configPath);
+    }
+
+    public List<String> tokenize() throws IOException {
+        String content = read();
+        List<String> tokens = new ArrayList<>();
+        StringBuilder current = new StringBuilder();
+
+        for (char c : content.toCharArray()) {
+            if (Character.isWhitespace(c)) {
+                addToken(tokens, current);
+            } else if (c == '{' || c == '}' || c == ';') {
+                addToken(tokens, current);
+                tokens.add(String.valueOf(c));
+            } else {
+                current.append(c);
+            }
+        }
+
+        addToken(tokens, current);
+        return tokens;
+    }
+
+    private void addToken(List<String> tokens, StringBuilder current) {
+        if (current.length() > 0) {
+            tokens.add(current.toString());
+            current.setLength(0);
+        }
     }
 }
