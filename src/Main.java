a543056b2e941cd3833b6a60e9a0cd08dabf9ea0
oqritel <othmanqritel@gmail.com>
2026-08-13 15:47:32 +0100

    read the configuration file

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
+import java.io.IOException;
+
+import config.ConfigLoader;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        
+        String configFilePath = args[0];
+        ConfigLoader configLoader = new ConfigLoader(configFilePath);
+
+        try {
+            String configContent = configLoader.read();
+            System.out.println("Config Content:");
+            System.out.println(configContent);
+        } catch (IOException e) {
+            System.err.println("Error reading config file: " + e.getMessage());
+            System.exit(1);
+        }
+    }
 }
--- src/config/ConfigLoader.java
+++ src/config/ConfigLoader.java
@@ -1,5 +1,17 @@
 package config;
 
-public class ConfigLoader {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class ConfigLoader {
+    private final Path configPath;
+
+    public ConfigLoader(String configPath) {
+        this.configPath = Path.of(configPath);
+    }
+
+    public String read() throws IOException {
+        return Files.readString(configPath);
+    }
 }
